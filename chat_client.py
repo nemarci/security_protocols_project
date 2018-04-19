c0d548d889dbc4077aa0d495a46918f1cec60ce4
Márton Páli <marci-95@hotmail.com>
2018-04-19 10:47:21 +0200

    Basic socket based chat program with tkinter UI

--- chat_client.py
+++ chat_client.py
@@ -1,2 +1,71 @@
 #!/usr/bin/env python
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 from chat_common import *
+import tkinter
+
+"Receiving message"
+def receive():
+    while True:
+        try:
+            "The max size of the message 1024 byte (?)"
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            "Show the message on the UI"
+            msg_list.insert(tkinter.END, msg)
+        except OSError:
+            break
+
+"Send message"
+def send(event=None):
+    "Get the message text from the textbox"
+    msg = my_msg.get()
+    my_msg.set("")
+    "We send the message using the socket which"
+    "is connected to one of the server's sockets"
+    client_socket.send(bytes(msg, "utf8"))
+    "Quit scenario"
+    if msg == "{quit}":
+        client_socket.close()
+        top.quit()
+
+"This method is called when the application is closing"
+def on_closing(event=None):
+    my_msg.set("{quit}")
+    send()
+
+"UI creation"
+top = tkinter.Tk()
+top.title("Chatter")
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg.set("Type your messages here.")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+"Application startup requires the user to type in the server's address and port"
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not PORT:
+    PORT = 33000  # Default value.
+else:
+    PORT = int(PORT)
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()  # Starts GUI execution.
--- chat_server.py
+++ chat_server.py
@@ -1,2 +1,69 @@
 #!/usr/bin/env python
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 from chat_common import *
+
+"We can store the user's names and addresses in these containers"
+clients = {}
+addresses = {}
+
+"Basic attributes of the server"
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+"Handle client connection"
+def accept_incoming_connections():
+    "We would like to use this method while the server is running"
+    "That's why it's an endless loop"
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected!" % client_address)
+        client.send(bytes("Greetings from the cave!\nNow type your name and press ENTER!", "utf8"))
+        addresses[client] = client_address
+        "Asynchronous client handling"
+        Thread(target=handle_client, args=(client, )).start()
+
+"Handle client after connection to the server"
+def handle_client(client):
+    name = client.recv(BUFSIZ).decode("utf8") #The user's name
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit' % name
+    "We can send message directly to the client"
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    "Or use this method, to broadcast it to every known (connected) client"
+    broadcast(bytes(msg, "utf8"))
+    "Store the user's name"
+    clients[client] = name
+    "This loop handles the messages of this particular client only"
+    "The server's always 'listening' to the client -> endless loop"
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            "Quit scenario"
+            client.send(bytes("{quit}", "utf8"))
+            "Closing the socket"
+            client.close()
+            "Deleting the data of the client from the storage"
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+"Sending a message to all connected user"
+def broadcast(msg, prefix=""):
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connections...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
